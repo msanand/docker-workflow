cd12cdfdd0da3aba0d6074d0b9c6bd68512785f8
Anand M.S <anand.m.s@sap.com>
2015-03-22 17:28:32 +0530

    Docker workflow linking node, redis, nginx

---
+++ node/index.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    http = require('http'),
+    redis = require('redis');
+
+var app = express();
+
+console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
+
+var client = redis.createClient(
+	process.env.REDIS_PORT_6379_TCP_PORT,
+  	process.env.REDIS_PORT_6379_TCP_ADDR
+);
+
+app.get('/', function(req, res, next) {
+  client.incr('counter', function(err, counter) {
+    if(err) return next(err);
+    res.send('This page has been viewed ' + counter + ' times!');
+  });
+});
+
+http.createServer(app).listen(process.env.PORT || 8080, function() {
+  console.log('Listening on port ' + (process.env.PORT || 8080));
+});
