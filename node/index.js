7d5ce685defa154a49b695f470f2728a33de938d
Anand M.S <anand.m.s@sap.com>
2015-04-03 10:47:22 +0530

    Altered Dockerfiles. Added diff approaches.

--- node/index.js
+++ node/index.js
@@ -6,10 +6,15 @@ var app = express();
 
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
-var client = redis.createClient(
-	process.env.REDIS_PORT_6379_TCP_PORT,
-  	process.env.REDIS_PORT_6379_TCP_ADDR
-);
+// APPROACH 1: Using environment variables created by Docker
+// var client = redis.createClient(
+// 	process.env.REDIS_PORT_6379_TCP_PORT,
+//   	process.env.REDIS_PORT_6379_TCP_ADDR
+// );
+
+// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+var client = redis.createClient('6379', 'redis');
+
 
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
