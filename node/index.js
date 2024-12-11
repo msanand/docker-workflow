1e2fad09550e910371854ff1b4454d4d82a99cd7
Anand Mani Sankar <anand.m.s@gmail.com>
2024-12-11 12:35:21 +0000

    Updated dependencies

--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
-var express = require('express'),
+const express = require('express'),
     http = require('http'),
     redis = require('redis');
 
-var app = express();
+const app = express();
 
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
@@ -13,7 +13,14 @@ console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_
 // );
 
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-var client = redis.createClient('6379', 'redis');
+// const client = redis.createClient('6379', 'redis');
+
+// Connect to Redis server
+const client = redis.createClient({
+  url: 'redis://localhost:6379',
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
 
 
 app.get('/', function(req, res, next) {
